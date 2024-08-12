157f097483f7c361274f9d93628dc4c7edb4d6f8
Toni Centrih <tcn@ordix.de>
2024-08-12 13:51:19 +0200

    Initial Commit

---
+++ sofle_v2/keymaps/tcn2/config.h
@@ -0,0 +1,24 @@
+/* Copyright 2023 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define LED_CAPS_LOCK_PIN 24
+#define LED_PIN_ON_STATE 0
+#define ENCODERS_PAD_A { B2 }
+#define ENCODERS_PAD_B { B6 }
+#define ENCODERS_PAD_A_RIGHT { B2 }
+#define ENCODERS_PAD_B_RIGHT { B6 }
---
+++ sofle_v2/rules.mk
@@ -0,0 +1,4 @@
+DEFAULT_FOLDER = splitkb/aurora/sofle_v2/rev1
+CONVERT_TO = liatris
+UNICODE_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
